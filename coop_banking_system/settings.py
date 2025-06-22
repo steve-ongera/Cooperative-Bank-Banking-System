bd3587378597bf76ef1cd942518a7f869314b67f
steve <gadafisteve001@gmail.com>
2025-06-22 11:46:13 +0300

    updates

--- coop_banking_system/settings.py
+++ coop_banking_system/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'coop_banking_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
